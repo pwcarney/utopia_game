aa014a0638a1a904ddc2c36c6548ad8f9b7aa61d
whatPanache <pwcarney@bu.edu>
2017-02-12 22:58:38 -0500

    Dudes find oil

---
+++ utopia_game/Assets/Scripts/WorkerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerController : MonoBehaviour 
+{
+	public float speed = 1f;
+
+	private GameObject seeking_oil_patch;
+	private GameObject seeking_collector;
+	private bool hasOil = false;
+
+	void Start () 
+	{
+		float oil_patch_distance = 10000f;
+		foreach(GameObject oil_patch in GameObject.FindGameObjectsWithTag("Oil"))
+		{
+			float dist_to_patch = Vector3.Distance (transform.position, oil_patch.transform.position);
+			if (dist_to_patch < oil_patch_distance) 
+			{
+				seeking_oil_patch = oil_patch;
+				oil_patch_distance = dist_to_patch;
+			}
+		}
+
+		float collector_distance = 10000f;
+		foreach(GameObject collector in GameObject.FindGameObjectsWithTag("Collector"))
+		{
+			float dist_to_collector = Vector3.Distance (transform.position, collector.transform.position);
+			if (dist_to_collector < collector_distance) 
+			{
+				seeking_collector = collector;
+				collector_distance = dist_to_collector;
+			}
+		}
+	}
+
+	void Update () 
+	{
+		if (!hasOil) 
+		{
+			transform.position = Vector2.MoveTowards (transform.position, seeking_oil_patch.transform.position, speed * Time.deltaTime);
+		} 
+		else 
+		{
+			transform.position = Vector2.MoveTowards (transform.position, seeking_collector.transform.position, speed * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.tag == "Oil") 
+		{
+			hasOil = true;
+		} 
+		else if (other.tag == "Collector")
+		{
+			hasOil = false;
+		}
+	}
+}
