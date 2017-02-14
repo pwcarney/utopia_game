0eae9f44476a0d83df54ebdd4360f6bec3f7874b
whatPanache <pwcarney@bu.edu>
2017-02-13 21:27:31 -0500

    Can place buildings which spawn workers who find oil

---
+++ utopia_game/Assets/Scripts/BuildingPlacementController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingPlacementController : MonoBehaviour {
+
+	bool storagePlacement = false;
+	public GameObject storagePrefab;
+	public Image storageButton;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0))  // Left click
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mousePos.z = 0;
+			if (storagePlacement) 
+			{
+				Instantiate(storagePrefab, mousePos, Quaternion.identity);
+				LeavePlacementMode ();
+			}
+		}
+
+		if (Input.GetMouseButtonDown (1))  // Right click
+		{
+			LeavePlacementMode ();
+		}
+	}
+
+	public void EnterStoragePlacementMode()
+	{
+		if (!storagePlacement) 
+		{
+			storagePlacement = true;
+
+			Color buttonColor = storageButton.color;
+			buttonColor.a -= 0.5f;
+			storageButton.color = buttonColor;
+		}
+	}
+
+	void LeavePlacementMode()
+	{
+		if (storagePlacement) 
+		{
+			storagePlacement = false;
+
+			Color buttonColor = storageButton.color;
+			buttonColor.a += 0.5f;
+			storageButton.color = buttonColor;
+		}
+	}
+}
---
+++ utopia_game/Assets/Scripts/StorageController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorageController : MonoBehaviour {
+
+	public GameObject workerPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SpawnWorker ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnMouseDown()
+	{
+		SpawnWorker ();
+	}
+
+	void SpawnWorker()
+	{
+		Vector3 spawnPosition = transform.position;
+		spawnPosition.y -= 0.5f;
+		Instantiate(workerPrefab, spawnPosition, Quaternion.identity);
+	}
+}
