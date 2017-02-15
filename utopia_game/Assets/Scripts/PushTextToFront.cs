03dd04fb9d4a6defdc08eaaa6bfe72a6d0420e9e
whatPanache <pwcarney@bu.edu>
2017-02-14 20:45:38 -0500

    This will make text stuff easier..

---
+++ utopia_game/Assets/Scripts/PushTextToFront.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushTextToFront : MonoBehaviour 
+{
+	public string layerToPushTo;
+
+	void Start () 
+	{
+		GetComponent<Renderer>().sortingLayerName = layerToPushTo;
+	}
+}
