9d09f3808dde9f8cdec5d64f187dfaa3c9fb385c
Mike <sawdust64@gmail.com>
2017-02-15 19:25:11 -0500

    start of oil count

--- utopia_game/Assets/Scripts/StorageController.cs
+++ utopia_game/Assets/Scripts/StorageController.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StorageController : MonoBehaviour {
 
 	public GameObject workerPrefab;
+	public Text OilCount;
+
+	private int oil;
 
 	// Use this for initialization
 	void Start () 
 	{
 		SpawnWorker ();
+		oil = 0;
+		SetOilCount ();
 	}
 	
 	// Update is called once per frame
@@ -28,5 +34,19 @@ public class StorageController : MonoBehaviour {
 		Vector3 spawnPosition = transform.position;
 		spawnPosition.y -= 0.5f;
 		Instantiate(workerPrefab, spawnPosition, Quaternion.identity);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Worker"))
+		{
+			oil = oil + 1;
+			SetOilCount ();
+		}
+	}
+
+	void SetOilCount ()
+	{
+		OilCount.text = "Oil: " + oil.ToString ();
 	}
 }
